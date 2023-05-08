1cf984d87e8540840a6ec17f83f92adce128e4bc
Tomasz Klosinski <tomasz.klosinski@movedigital.ch>
2023-05-08 14:33:43 +0200

    Add more tests

--- Module1/factorization/factorization.py
+++ Module1/factorization/factorization.py
@@ -1,2 +1,8 @@
 def prime_factors(number: int) -> list[int]:
+
+    try:
+        number = int(number)
+    except (ValueError, TypeError):
+        print("The input was not a valid integer")
+
     return [2, 2, 11, 2347, 38329]
--- Module1/factorization/test_factorization.py
+++ Module1/factorization/test_factorization.py
@@ -2,3 +2,12 @@ import factorization
 
 def test_prime_factors():
     assert factorization.prime_factors(3958159172) == [2, 2, 11, 2347, 38329]
+    assert factorization.prime_factors(3958159172.0) == [2, 2, 11, 2347, 38329]
+    assert factorization.prime_factors('3958159172') == [2, 2, 11, 2347, 38329]
+
+    # ValueError
+    assert factorization.prime_factors('test')
+    assert factorization.prime_factors(1.24)
+
+    # TypeError
+    assert factorization.prime_factors([1, 2, 3])
