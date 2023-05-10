cc1d115639be7a95c07442a4dc899ef05de8e882
Tomasz Klosinski <tomasz.klosinski@movedigital.ch>
2023-05-10 16:47:42 +0200

    Add implementation of to_roman()

--- Module1/Module2/numeral_systems/converter.py
+++ Module1/Module2/numeral_systems/converter.py
@@ -1,5 +1,27 @@
 def to_roman(number: int) -> str:
-    pass
+    mumber = int(number)
+
+    if number < 1:
+        return None
+
+    else:
+        m = ["", "M", "MM", "MMM"]
+        c = ["", "C", "CC", "CCC", "CD", "D",
+            "DC", "DCC", "DCCC", "CM "]
+        x = ["", "X", "XX", "XXX", "XL", "L",
+            "LX", "LXX", "LXXX", "XC"]
+        i = ["", "I", "II", "III", "IV", "V",
+            "VI", "VII", "VIII", "IX"]
+
+        thousands = m[number // 1000]
+        hundreds = c[(number % 1000) // 100]
+        tens = x[(number % 100) // 10]
+        ones = i[number % 10]
+
+        roman = thousands + hundreds + tens + ones
+
+        return roman.replace(' ', '')
+
 
 def from_roman(string: str) -> int:
     pass
